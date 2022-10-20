85c967b3ab346ae1d4bc0c1e131aeffe7b98de8b
Christoffer Jansson <christofferjansson1@gmail.com>
2022-10-20 10:48:08 +0200

    update

---
+++ Ex7_MenuSystem/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuSystem
+{
+    internal class Menu
+    {
+        public string Title;
+        public string MenuItems;
+        MenuItem menuItems = new MenuItem();
+        public int Itemcount = 0;
+
+        public Show()
+        {
+
+        }
+    }
+}
