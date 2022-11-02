65d90d6a9e499474515f215f057400b70497bdc9
Christoffer <christofferjansson1@gmail.com>
2022-11-02 09:34:05 +0100

    update

--- Properties demo/Program.cs
+++ Properties demo/Program.cs
@@ -4,6 +4,9 @@ namespace Properties_demo
 {
     internal class Program
     {
+        //property =    A property is a member that provides a flexible mechanism to read, 
+        //              write, or compute the value of a private field. 
+
         static void Main(string[] args)
         {
             Console.WriteLine("This is exampel 1:");
