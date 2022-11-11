975d10c0be47363e1a1d355024c8c627f05960be
Christoffer <christofferjansson1@gmail.com>
2022-11-11 08:59:59 +0100

    update laptop

---
+++ ex22/ex22/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ex22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //StreamReader sr = new StreamReader("test.txt");
+            //try
+            //{
+
+            //        string line;
+            //        // Read and display lines from the file until the end of
+            //        // the file is reached.
+            //        while ((line = sr.ReadLine()) != null)
+            //        {
+            //            Console.WriteLine(line);
+            //        }
+                
+            //}
+            //finally
+            //{
+            //    sr.Close();
+            //}
+            string fileName = "test.txt";
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line;
+                // Read and display lines from the file until the end of
+                // the file is reached.
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
